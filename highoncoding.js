49512158c81ea7ab00a6aa04a8fdb5bede3fd404
Alex Le <alexle@Alexs-MBP-2.localdomain>
2018-10-08 16:00:18 -0500

    October 8 java script assignment

---
+++ highoncoding.js
@@ -0,0 +1,83 @@
+
+
+ let element = document.createElement("div")
+ element.className = "headline"
+
+        let menu = document.createElement("menu")
+        menu.className = "menu"
+        menu.textContent = "High On Coding       Home      Categories"
+        element.appendChild(menu)
+
+
+       
+
+        let header = document.createElement("h1")
+        header.className = "h1"
+       header.textContent = "Curse of Current Reviews"
+       element.appendChild(header)
+
+
+        let paragraph = document.createElement("p")
+        paragraph.className = "app_container"
+        paragraph.textContent ="When you want to buy any application from the Apple iTunes store you have more choices than you can handle.Most of the users scroll past the application description completely avoiding it like ads displayed on the right column of  your webpage. Their choice is dependent on three important factors price, screenshot and reviews."
+        element.appendChild(paragraph)
+        
+        let header2 = document.createElement("h2")
+        header2.className = "app_container_header2"
+        header2.textContent = "Hello Watchkit"
+        element.appendChild(header2)
+
+
+        let linebreak = document.createElement("div")
+        linebreak.className = "headline2"
+        element.appendChild(linebreak)
+        
+
+       
+
+       let paragraph2 = document.createElement("p2")
+       paragraph2.className = "app_container2"
+       paragraph2.textContent = "Last month Apple released the anticipated WatchKit Framework for developers in the for of iOS 8.2 beta SDK release. The WatchKit framework enables the developers to create Apple Watch applications. In this article, we are going to focus on the basics of getting started with the WatchKit framework and developing apps for the Apple Watch."
+       element.appendChild(paragraph2)
+
+        
+
+        let commentbar1 = document.createElement("h4")
+        commentbar1.className = "h4"
+        commentbar1.textContent = "12 comments          24 likes"
+        element.appendChild(commentbar1)
+
+
+        let header3 = document.createElement("h3")
+        header3.className = "app_container_header3"
+        header3.textContent = "Introduction to Swift"
+        element.appendChild(header3)
+
+        let paragraph3 = document.createElement("p3")
+        paragraph3.className = "app_container3"
+        paragraph3.textContent = "Swift is a modern programming language developed by Apple to create the next generation of iOS and OSX applications. Swift is a fairly new language and still in development but it clearly reflects the intentions and the future direction. This article will revolve around the basic concepts in the Swift language and how you can get started."
+        element.appendChild(paragraph3)
+
+
+        let commentbar2 = document.createElement("h5")
+        commentbar2.className = "h4"
+        commentbar2.textContent = "15 comments          45 likes"
+        element.appendChild(commentbar2)
+
+
+        container.appendChild(element)
+       
+      
+        
+
+        
+    
+        
+
+
+
+
+
+
+
+
